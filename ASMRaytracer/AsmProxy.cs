b6a1a48ca24ed6c66646322eda4c5b47cac6ade1
JezewskiG <gjwsky@icloud.com>
2021-12-03 20:34:16 +0100

    [Dot] Working dot product assembly

--- ASMRaytracer/AsmProxy.cs
+++ ASMRaytracer/AsmProxy.cs
@@ -8,24 +8,21 @@ namespace AplClient
 {
     public unsafe class AsmProxy
     {
-        //Dll import pattern.
-        [DllImport("..\\AsmLibrary.dll")]
-        private static extern double asmAddTwoDoublesTwo(double a, double b);
         
         [DllImport("..\\AsmLibrary.dll")]
-        private static extern double asmDotProduct(Vector4 first, Vector4 second);
+        private static extern float dotProduct(Vector4 first, Vector4 second);
 
-
-        public double executeAsmAddTwoDoubles(double a, double b)
-        {
-            return asmAddTwoDoublesTwo(a, b);
-        }
 
         public float executeDotProduct(Vector3 first, Vector3 second)
         {
+            //Vector4 test1 = new Vector4(1.14214f, 2.0f, 43.0f, 0.0f);
+            //Vector4 test2 = new Vector4(0.02221f, 2.0f, 3.0f, 0.0f);
+            
             Vector4 firstt = new Vector4(first, 0.0f);
             Vector4 secondt = new Vector4(second, 0.0f);
-            return (float)asmDotProduct(firstt, secondt);
+            float calcDot = dotProduct(firstt, secondt);
+            //float calcDotVector = Vector4.Dot(test1, test2);
+            return calcDot;
         }
 
         public bool executeAsmIntersect(Ray inRay, float timeMin, ref float timeMax, Vector3 center, float radius)
@@ -44,10 +41,10 @@ namespace AplClient
             {
                 float deltaSqrt = MathF.Sqrt(delta);
 
-                //root = (-b - deltaSqrt) / a;
+                root = (-b - deltaSqrt) / a;
                 if (root > timeMax || root < timeMin)
                 {
-                    //root = (-b + deltaSqrt) / a;
+                    root = (-b + deltaSqrt) / a;
                     if (root > timeMax || root < timeMin)
                     {
                         return false;
--- AsmLibrary/AsmLibrary.asm
+++ AsmLibrary/AsmLibrary.asm
@@ -3,24 +3,16 @@ _zero_vector real4 0.0, 0.0, 0.0, 0.0
 
 .code
 
-asmAddTwoDoubles proc
-	vaddpd ymm0, ymm0, ymm1
-	ret
-asmAddTwoDoubles endp
-
-asmAddTwoDoublesTwo proc
-	vmulpd ymm0, ymm0, ymm1
-	ret
-asmAddTwoDoublesTwo endp
-
-
-asmDotProduct proc
-	; YMM0 and YMM1 contain two input Vector4's
+dotProduct PROC
+	; Save return pointer on the stack
+	push rbp
+	mov rbp, rsp
+	; XMM0 and XMM1 contain two input Vector4's
 	;
-	; Multiplying two Vector4's with double precision, storing results in XMM0.
+	; Multiplying two Vector4's with single precision, storing results in XMM0.
 	vmulps xmm0, xmm0, xmm1
 	;
-	; Taking two highest floats from XMM0 to lower part of XMM1
+	; Taking two highest packed floats from XMM0 to lower part of XMM1
 	movhlps xmm1, xmm0
 	;
 	; Adding two highest and two lowest floats, [XYZW] -> X+Z, Y+W
@@ -35,7 +27,54 @@ asmDotProduct proc
 	; Add lowest [Y+W]+[X+Z]
 	addps xmm0, xmm1
 	;
+	; Pop the return address off the stack
+	pop rbp
+	;
+	; Return to caller
 	ret
-asmDotProduct endp
+dotProduct ENDP
+
+asmSphereIntersect PROC
+	; Sphere intersection function returning boolean value after determining if collision happens.
+	;---------------------------
+	; Inputs:
+	;	- XMM0: Ray origin
+	;	- XMM1: Ray direction
+	;	- XMM2: Sphere origin
+	;	- XMM3: Time max
+	;	- On stack: Sphere radius
+	;
+	; Returns:
+	;	Value on XMM0
+	;	- Time value of intersection found if > 0 
+	;	- Value of -1 if intersection is not possible
+	;
+	;---------------------------- 
+
+	; Vector3 originC = inRay.origin - this.center: Result in XMM0 (inRay.origin can be overwritten)
+	vsubps xmm0, xmm0, xmm2
+	;
+	; float a = inRay.direct.LengthSquared()
+	; TODO
+	;
+	; float b = Vector3.Dot(inRay.direct, originC)
+	call dotProduct
+	;
+	; float c = originC.LengthSquared() - (radius*radius)
+	; TODO
+	; 
+	; float delta= b*b-4*a*c
+	; TODO
+	;
+	; if(delta < 0.0) return false (-1)
+	; TODO
+	;
+	; else ...
+
+
+asmSphereIntersect ENDP
+
+
+
 
 end
