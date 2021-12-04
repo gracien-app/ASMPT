7581ef8ee963e6132332fda63fab626f914eee2e
JezewskiG <gjwsky@icloud.com>
2021-12-04 20:26:26 +0100

    [ASM] Working Delta ASM calculations.

--- ASMRaytracer/AsmProxy.cs
+++ ASMRaytracer/AsmProxy.cs
@@ -8,32 +8,38 @@ namespace AplClient
 {
     public unsafe class AsmProxy
     {
-        
+
         [DllImport("..\\AsmLibrary.dll")]
-        private static extern float dotProduct(Vector4 first, Vector4 second);
-
-
-        public float executeDotProduct(Vector3 first, Vector3 second)
-        {
-            //Vector4 test1 = new Vector4(1.14214f, 2.0f, 43.0f, 0.0f);
-            //Vector4 test2 = new Vector4(0.02221f, 2.0f, 3.0f, 0.0f);
-            
-            Vector4 firstt = new Vector4(first, 0.0f);
-            Vector4 secondt = new Vector4(second, 0.0f);
-            float calcDot = dotProduct(firstt, secondt);
-            //float calcDotVector = Vector4.Dot(test1, test2);
-            return calcDot;
-        }
+        private static extern float asmSphereIntersect(Vector4 first, Vector4 second, 
+                                                       Vector4 three, Vector4 radius);
 
         public bool executeAsmIntersect(Ray inRay, float timeMin, ref float timeMax, Vector3 center, float radius)
         {
+            //Vector4 origin = new Vector4(0.0f, 1.0f, 1.0f, 0.0f);
+            //Vector4 direction = new Vector4(0.5f, 0.5f, 0.5f, 0.0f);
+            //Vector4 fcenter = new Vector4(1.0f, 1.0f, 1.0f, 0.0f);
+            //Vector4 fradius = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+
+
+            //Vector4 originC = origin - fcenter;
+            //float a = direction.LengthSquared();
+            //float b = Vector4.Dot(direction, originC);
+            //////float b = executeDotProduct(inRay.direct, originC);
+            //float c = originC.LengthSquared() - (fradius.X * fradius.X);
+
             Vector3 originC = inRay.origin - center;
             float a = inRay.direct.LengthSquared();
-            float b = executeDotProduct(inRay.direct, originC);
-            float c = (originC).LengthSquared() - (radius * radius);
+            float b = Vector3.Dot(inRay.direct, originC);
+            float c = originC.LengthSquared() - (radius * radius);
 
-            float delta = b * b - a * c;
+            float delta = asmSphereIntersect(new Vector4(inRay.origin, 0.0f), 
+                                             new Vector4(inRay.direct, 0.0f), 
+                                             new Vector4(center, 0.0f), 
+                                             new Vector4(radius));
 
+            //float delta = asmSphereIntersect(origin, direction, fcenter, fradius);
+
+            //float delta = (b * b) - (4 * a * c);
             float root = 0.0f;
 
             if (delta < 0.0) return false;
--- AsmLibrary/AsmLibrary.asm
+++ AsmLibrary/AsmLibrary.asm
@@ -1,13 +1,102 @@
 .data
-_zero_vector real4 0.0, 0.0, 0.0, 0.0
+align 16
+_arg_one real8 ?
+align 16
+_arg_two real8 ?
+align 16
+_arg_three real8 ?
+align 16
+_arg_four real4 ?
+
+align 16
+_adelta real4 ?
+_bdelta real4 ?
+_cdelta real4 ?
+align 16
+_input_one real8 ?
+align 16
+_input_two real8 ?
 
 .code
 
+asmSphereIntersect PROC
+	; Sphere intersection function returning boolean value after determining if collision happens.
+	;---------------------------
+	; Inputs:
+	;	- XMM0: Ray origin (OriginC)
+	;	- XMM1: Ray direction
+	;	- XMM2: Sphere center
+	;	- XMM3: Sphere radius
+	;	- On stack: Time max
+	;
+	; Returns:
+	;	Value on XMM0
+	;	- Time value of intersection found if > 0 
+	;	- Value of -1 if intersection is not possible
+	;
+	;---------------------------- 
+	;
+	; Vector3 originC = inRay.origin - this.center: Result in XMM0 (inRay.origin can be overwritten)
+	vsubps xmm0, xmm0, xmm2
+	;
+	; Save constant arguments in variables
+	movupd [_arg_one], xmm0
+	movupd [_arg_two], xmm1
+	movupd [_arg_three], xmm2
+	movss [_arg_four], xmm3
+	;
+	; Save save corrected origin-center vector as first argument (inRay.origin no longer needed)
+	movupd [_arg_one], xmm0
+	;
+	; float a = inRay.direct.LengthSquared()
+	movupd [_input_one], xmm1
+	movupd [_input_two], xmm1
+	call dotProduct
+	movss [_adelta], xmm0
+	;
+	; float b = Vector3.Dot(inRay.direct, originC): Result in XMM4
+	; mov r8, offset _arg_one
+	; push r8
+	movupd xmm0, qword ptr [_arg_one]
+	movupd xmm1, qword ptr [_arg_two]
+	movupd [_input_one], xmm0
+	movupd [_input_two], xmm1
+	call dotProduct
+	movss [_bdelta], xmm0
+	;
+	; float c = originC.LengthSquared() - (radius*radius)
+	movupd xmm0, qword ptr [_arg_one]
+	movupd [_input_one], xmm0
+	movupd [_input_two], xmm0
+	call dotProduct
+	movss xmm1, dword ptr [_arg_four]
+	mulps xmm1, xmm1
+	vsubps xmm0, xmm0, xmm1
+	movss [_cdelta], xmm0
+	; TODO
+	; 
+	; float delta= b*b-a*c 
+	; YES ITS WITHOUT 4 DONT CORRECT PLS NO I HAVENT GOT WRONG RESULTS FOR 6 HOURS BECAUSE I'VE HAD 4*a*c THERE
+	movss xmm0, dword ptr [_bdelta]
+	mulps xmm0, xmm0
+	movss xmm1, dword ptr [_adelta]
+	movss xmm2, dword ptr [_cdelta]
+	mulps xmm1, xmm2
+	vsubps xmm0, xmm0, xmm1
+	;
+	; ...
+	; TODO
+	; ...
+	;
+	ret
+asmSphereIntersect ENDP
+
 dotProduct PROC
-	; Save return pointer on the stack
-	push rbp
-	mov rbp, rsp
-	; XMM0 and XMM1 contain two input Vector4's
+	
+	movupd xmm0, qword ptr [_input_one]
+	movupd xmm1, qword ptr [_input_two]
+	;
+	; XMM4 and XMM5 contain two input Vector4's
 	;
 	; Multiplying two Vector4's with single precision, storing results in XMM0.
 	vmulps xmm0, xmm0, xmm1
@@ -27,54 +116,8 @@ dotProduct PROC
 	; Add lowest [Y+W]+[X+Z]
 	addps xmm0, xmm1
 	;
-	; Pop the return address off the stack
-	pop rbp
-	;
 	; Return to caller
 	ret
 dotProduct ENDP
-
-asmSphereIntersect PROC
-	; Sphere intersection function returning boolean value after determining if collision happens.
-	;---------------------------
-	; Inputs:
-	;	- XMM0: Ray origin
-	;	- XMM1: Ray direction
-	;	- XMM2: Sphere origin
-	;	- XMM3: Time max
-	;	- On stack: Sphere radius
-	;
-	; Returns:
-	;	Value on XMM0
-	;	- Time value of intersection found if > 0 
-	;	- Value of -1 if intersection is not possible
-	;
-	;---------------------------- 
-
-	; Vector3 originC = inRay.origin - this.center: Result in XMM0 (inRay.origin can be overwritten)
-	vsubps xmm0, xmm0, xmm2
-	;
-	; float a = inRay.direct.LengthSquared()
-	; TODO
-	;
-	; float b = Vector3.Dot(inRay.direct, originC)
-	call dotProduct
-	;
-	; float c = originC.LengthSquared() - (radius*radius)
-	; TODO
-	; 
-	; float delta= b*b-4*a*c
-	; TODO
-	;
-	; if(delta < 0.0) return false (-1)
-	; TODO
-	;
-	; else ...
-
-
-asmSphereIntersect ENDP
-
-
-
 
 end
